892e66089c4d8df0cb945a8404098838aa0525f5
xwjkka <hurmuzz12@gmail.com>
2023-06-12 18:02:14 +0300

    struct-done

--- ToDo/ToDo/Domain/TodoItem.swift
+++ ToDo/ToDo/Domain/TodoItem.swift
@@ -1,32 +1,44 @@
+import Foundation
+
 enum Importance {
     case unimportant;
     case regular;
     case important;
 }
 
-extension TodoItem {
-    var json: Any
-    
-    static func parse(json: Any) -> TodoItem? {
-        guard let parsedJson = JSONSerialization.jsonObject(with: json, options: []) as? [String: Any] {
-            return
-        }
-        
-        let item = TodoItem(text: parsedJson[text], deadline: parsedJson[], done: parsedJson, created: parsedJson, changed: parsedJson[])
-    }
-    
-}
+//extension TodoItem {
+//    var json: Any
+//
+//    static func parse(json: Any) -> TodoItem? {
+//        guard let parsedJson = JSONSerialization.jsonObject(with: json, options: []) as? [String: Any] {
+//            return
+//        }
+//
+//        let item = TodoItem(text: parsedJson[text], deadline: parsedJson[], done: parsedJson, created: parsedJson, changed: parsedJson[])
+//    }
+//
+//}
 
 struct TodoItem {
-    let id: Int = UUID().uuidString
+    let id: String
     let text: String
-    let importance: Importance = regular
+    let importance: Importance
     let deadline: Date?
     let done: Bool
     let created: Date
     let changed: Date?
+    
+    init(id: String = UUID().uuidString, text: String, importance: Importance, deadline: Date? = nil, done: Bool = false, created: Date = Date(), changed: Date? = nil) {
+            self.id = id
+            self.text = text
+            self.importance = importance
+            self.deadline = deadline
+            self.done = done
+            self.created = created
+            self.changed = changed
+    }
 }
 
-class FileCache {
-    
-}
+//class FileCache {
+//
+//}
