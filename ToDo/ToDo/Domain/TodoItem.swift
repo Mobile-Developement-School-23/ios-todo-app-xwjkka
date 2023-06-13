5257b82ca62cd81810c370c2f7b2707245a29f76
Jeanna Krysten <jeannakr@mi-i6.msk.21-school.ru>
2023-06-13 16:45:13 +0300

    parse only data

--- ToDo/ToDo/Domain/TodoItem.swift
+++ ToDo/ToDo/Domain/TodoItem.swift
@@ -6,18 +6,26 @@ enum Importance {
     case important;
 }
 
-//extension TodoItem {
+extension TodoItem {
 //    var json: Any
-//
-//    static func parse(json: Any) -> TodoItem? {
-//        guard let parsedJson = JSONSerialization.jsonObject(with: json, options: []) as? [String: Any] {
-//            return
-//        }
-//
-//        let item = TodoItem(text: parsedJson[text], deadline: parsedJson[], done: parsedJson, created: parsedJson, changed: parsedJson[])
-//    }
-//
-//}
+
+    static func parse(json: Data) -> TodoItem? {
+//        if let jsonString =
+        do {
+            if let json = try JSONSerialization.jsonObject(with: json, options: []) as? [String: Any] {
+                if let text = json["text"] as? String {
+                    print(text)
+                }
+            }
+        } catch let error as NSError {
+            print("Failed to load: \(error.localizedDescription)")
+        }
+//        let item = TodoItem(text: json[text], deadline: json[dea], done: json, created: json, changed: nil)
+        let item = TodoItem.init(text: "String", importance: Importance.important, deadline: nil, done: true, created: nil, changed: nil)
+        return item
+    }
+
+}
 
 struct TodoItem {
     let id: String
@@ -25,10 +33,11 @@ struct TodoItem {
     let importance: Importance
     let deadline: Date?
     let done: Bool
-    let created: Date
+//    let created: Date
+    let created: Date?
     let changed: Date?
     
-    init(id: String = UUID().uuidString, text: String, importance: Importance, deadline: Date? = nil, done: Bool = false, created: Date = Date(), changed: Date? = nil) {
+    init(id: String = UUID().uuidString, text: String, importance: Importance, deadline: Date? = nil, done: Bool = false, created: Date? = nil, changed: Date? = nil) {
             self.id = id
             self.text = text
             self.importance = importance
@@ -42,3 +51,7 @@ struct TodoItem {
 //class FileCache {
 //
 //}
+let j = "{\"id\": \"12345\",\"text\": \"Сделать домашнее задание\", \"importance\": \"важная\", \"deadline\": \"2022-05-30T12:00:00Z\", \"isDone\": false, \"creationDate\": \"2022-05-28T08:30:00Z\", \"modificationDate\": \"2022-05-29T14:20:00Z\" }"
+let data = Data(j.utf8)
+let item = TodoItem.parse(json: data)
+print(item)
