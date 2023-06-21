632dd78a2b602a399b581034a974d652566562c4
xwjkka <hurmuzz12@gmail.com>
2023-06-21 18:21:22 +0300

    done button

--- ToDo/ToDo/ViewController.swift
+++ ToDo/ToDo/ViewController.swift
@@ -6,11 +6,29 @@ class ViewController: UIViewController {
         let headerLabel = UILabel()
         headerLabel.text = "Мои дела"
         headerLabel.font = UIFont.systemFont(ofSize: 34, weight: .bold)
-        headerLabel.textColor = UIColor.black
+        headerLabel.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
         
         return headerLabel
     }()
 
+    private let addButton: UIButton = {
+        let addButton = UIButton()
+        addButton.backgroundColor = #colorLiteral(red: 0, green: 0.4780646563, blue: 0.9985368848, alpha: 1)
+        
+        addButton.setTitle("+", for: .normal)
+        addButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 22)
+        addButton.titleLabel?.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        addButton.contentMode = .center
+        
+        
+        addButton.layer.shadowColor = #colorLiteral(red: 0, green: 0.4780646563, blue: 0.9985368848, alpha: 1)
+        addButton.layer.shadowOffset = CGSize(width: 0.0, height: 8.0)
+        addButton.layer.shadowOpacity = 0.3
+        addButton.layer.shadowRadius = 20
+        
+        addButton.layer.cornerRadius = 22
+        return addButton
+    }()
 
     private lazy var scrollView: UIScrollView = {
         let scrollView = UIScrollView()
@@ -22,7 +40,6 @@ class ViewController: UIViewController {
     
     private lazy var contentView: UIView = {
         let contentView = UIView()
-//        contentView.backgroundColor = #colorLiteral(red: 0.1215686277, green: 0.01176470611, blue: 0.4235294163, alpha: 1)
         contentView.frame.size = contentSizeMini
         contentView.layer.cornerRadius = 16;
         contentView.layer.masksToBounds = true;
@@ -34,17 +51,44 @@ class ViewController: UIViewController {
         stackView.backgroundColor = #colorLiteral(red: 0.969507277, green: 0.9645401835, blue: 0.9516965747, alpha: 1)
         stackView.axis = .vertical
         stackView.alignment = .center
-        stackView.spacing = 20
         
         return stackView
     }()
     
+    
+    private lazy var contentDoneView: UIView = {
+        let contentView = UIView()
+        
+        return contentView
+    }()
+    
+    private let doneLabel: UILabel = {
+        let headerLabel = UILabel()
+        headerLabel.text = "Выполнено - "
+        headerLabel.font = UIFont.systemFont(ofSize: 15, weight: .medium)
+        headerLabel.textColor = #colorLiteral(red: 0.6274510622, green: 0.6274510026, blue: 0.6274510026, alpha: 1)
+        
+        return headerLabel
+    }()
+    
+    private let showDoneButton: UIButton = {
+        let showDoneButton = UIButton()
+        showDoneButton.setTitle("Показать", for: .normal)
+        showDoneButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 15)
+        showDoneButton.titleLabel?.textColor = #colorLiteral(red: 0, green: 0.4780646563, blue: 0.9985368848, alpha: 1)
+        showDoneButton.contentMode = .right
+        return showDoneLabel
+    }()
+    
+    
+    
+    
     private var contentSize: CGSize {
-        CGSize(width: view.frame.width , height: view.frame.height + 400)  // было h + 400
+        CGSize(width: view.frame.width , height: view.frame.height)
     }
     
     private var contentSizeMini: CGSize {
-        CGSize(width: view.frame.width - 30 , height: view.frame.height + 400)  // было h + 400
+        CGSize(width: view.frame.width - 30 , height: view.frame.height)
     }
     
     override func viewDidLoad() {
@@ -55,11 +99,19 @@ class ViewController: UIViewController {
         view.addSubview(scrollView)
         scrollView.addSubview(contentView)
         contentView.addSubview(stackView)
+        
         view.addSubview(headerLabel)
+        
+        view.addSubview(contentDoneView)
+        contentDoneView.addSubview(doneLabel)
+        contentDoneView.addSubview(showDoneButton)
+        
+        view.addSubview(addButton)
 
         setupColors()
         setupHeaderConstraints()
         setupViewsConstraints()
+        setupAddButtonConstraints()
     }
 
 
@@ -81,7 +133,6 @@ extension ViewController {
             scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             scrollView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 15),
             scrollView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 15)
-//            scrollView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
         
         for view in stackView.arrangedSubviews {
@@ -101,6 +152,21 @@ extension ViewController {
             headerLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20)
         ])
 
+    }
+    
+    private func setupAddButtonConstraints() {
+        addButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            addButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            addButton.widthAnchor.constraint(equalToConstant: 44),
+            addButton.heightAnchor.constraint(equalToConstant: 44),
+            addButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50),
+        ])
+    }
+    
+    private func setupDoneViewConstraints() {
+        
     }
     
     private func setupColors() {
