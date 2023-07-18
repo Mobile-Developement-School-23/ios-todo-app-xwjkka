c8b6f5bcb19dc3a269c2df4c5b53cc5a1d6718df
xwjkka <hurmuzz12@gmail.com>
2023-07-18 17:45:12 +0300

    lets begin

---
+++ ToDo/ToDoSwiftUI/ToDoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoSwiftUIApp.swift
+//  ToDoSwiftUI
+//
+//  Created by Olesya Khurmuzakiy on 18.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
