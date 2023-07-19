bbfd1a051af5ec6dbd4780d43ef753ea8f7a65e3
xwjkka <hurmuzz12@gmail.com>
2023-07-19 16:43:21 +0300

    ppp

--- ToDo/ToDoSwiftUI/ContentView.swift
+++ ToDo/ToDoSwiftUI/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
 //    @State private var list = FileCache()
 //    private var list = FileCache()
-    var list = [TodoItem(text: "haha", importance: Importance.regular, created: Date()),
+    @State private var list = [TodoItem(text: "haha", importance: Importance.regular, created: Date()),
                 TodoItem(text: "again haha", importance: Importance.unimportant, deadline: Date(), created: Date()), TodoItem(text: "not haha", importance: Importance.regular, created: Date()), TodoItem(text: "haha", importance: Importance.important, created: Date())]
     private var countDone = 0
 
@@ -31,8 +31,17 @@ struct ContentView: View {
                                        Toggle(isOn: $isOn) { }
                                        .toggleStyle(iOSCheckboxToggleStyle())
 
-                                       VStack {
+                                       VStack(alignment: .leading) {
                                            Text("\(item.text)")
+                                           if let deadline = item.deadline {
+                                               HStack {
+                                                   Image(systemName: "calendar")
+                                                       .foregroundColor(.gray)
+                                                   Text(DateFormatter.DateFormatter.string(from: deadline))
+                                                       .foregroundColor(.gray)
+                                                       .padding(.leading, -5)
+                                               }
+                                           }
                                        }
                                    }
                                    .swipeActions(edge: .leading, allowsFullSwipe: true) {
@@ -71,6 +80,7 @@ struct ContentView: View {
                                    Button("Показать") {
                                        showDone.toggle()
                                    }
+//                                   .font(15)
                                }
                            } footer: {
                                    Text("©xwjkka")
@@ -90,7 +100,7 @@ struct ContentView: View {
 
                        }
                        .sheet(isPresented: $isNavigationViewPresented) {
-                           NavigationViewWithButtons()
+                           NavigationViewWithButtons(list: $list)
                        }
                    }
                }
@@ -107,13 +117,15 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 struct NavigationViewWithButtons: View {
+    @Binding var list: [TodoItem]
+//    @Binding var id: Int?
+    
     @State private var isSaveButtonPressed = false
     @State private var isCancelButtonPressed = false
     
     @State private var isDeadlineOn = false
     @State private var isCalendarVisible = false
     
-//    private var todoText = ""
     @State private var selectedImportance = 1
 
     @State var todoText: String = ""
@@ -126,72 +138,18 @@ struct NavigationViewWithButtons: View {
             ScrollView {
                 VStack {
                     TextField("Что делать?", text: $todoText)
-                        .frame(minHeight: 120)
+//                        .frame(minHeight: 120)
                         .padding()
                         .background(Color.white)
                         .cornerRadius(10)
                         .shadow(radius: 10)
                     VStack {
-                        HStack {
-                            Text("Важность")
-                            Spacer()
-                            
-                            HStack {
-                                Button(action: {
-                                    selectedImportance = 0
-                                }) {
-                                    Image(systemName: "arrow.down")
-                                    //                                    .foregroundColor(.gray)
-                                        .foregroundColor(selectedImportance == 2 ? .gray : .red)
-                                }
-                                .padding(2)
-                                
-                                Button(action: {
-                                    selectedImportance = 1
-                                }) {
-                                    Text("нет")
-                                        .foregroundColor(selectedImportance == 1 ? .black : .black)
-                                }
-                                .padding(2)
-                                
-                                Button(action: {
-                                    selectedImportance = 2
-                                }) {
-                                    Image(systemName: "exclamationmark.2")
-                                    //                                    .foregroundColor(.red)
-                                        .foregroundColor(selectedImportance == 2 ? .red : .gray)
-                                }
-                                .padding(2)
-                            }
-                            .shadow(color: .blue, radius: 10)
-                        } .padding([.top, .leading, .trailing])
-                        Toggle(isOn: $isDeadlineOn) {
-                            Text("Сделать до")
-                            if isDeadlineOn {
-                                Button(action: {
-                                    isCalendarVisible.toggle()
-                                }) {
-                                    Text(DateFormatter.DateFormatter.string(from: deadlineDate))
-                                }
-                            }
-                        } .padding([.bottom, .leading, .trailing])
-                        
-                        if isCalendarVisible {
-                            DatePicker(
-                                selection: $deadlineDate,
-                                displayedComponents: [.date],
-                                label: {}
-                            )
-                            .padding(.top, -20)
-                            .padding([.leading, .trailing])
-                            .datePickerStyle(GraphicalDatePickerStyle())
-                        }
-                                                
-                    }
-                    .background(Color.white)
-                    .cornerRadius(10)
-                    .shadow(radius: 10)
-                    .padding(.vertical)
+                        ImportanceView(selectedImportance: $selectedImportance)
+                        DeadlineView(isDeadlineOn: $isDeadlineOn, isCalendarVisible: $isCalendarVisible, deadlineDate: $deadlineDate)
+                    } .background(Color.white)
+                        .cornerRadius(10)
+                        .shadow(radius: 10)
+                        .padding(.vertical)
                     
                     Button(action: {
                         self.isCancelButtonPressed = true
@@ -200,9 +158,10 @@ struct NavigationViewWithButtons: View {
                             .foregroundColor(.red)
                             .padding()
                     }
-                    .frame(maxWidth: .infinity) // Задаем кнопке ширину во весь экран
-                    .background(Color.gray)
-                    .cornerRadius(10)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.white)
+                        .cornerRadius(10)
+                        .shadow(radius: 10)
                     Spacer()
                 }
                 .padding()
@@ -217,12 +176,78 @@ struct NavigationViewWithButtons: View {
                 )
                 .navigationBarItems(trailing:
                     Button(action: {
-                        self.isSaveButtonPressed = true
+                    var deadlineForItem: Date?
+                    if isDeadlineOn {
+                        deadlineForItem = deadlineDate
+                    }
+                    var importance = Importance.regular
+                    if selectedImportance == 0 {
+                        importance = .unimportant
+                    } else if selectedImportance == 2 {
+                        importance = .important
+                    }
+                    let item = TodoItem(text: todoText, importance: importance, deadline: deadlineForItem, done: false, created: Date())
+                    list.append(item)
+                    presentationMode.wrappedValue.dismiss()
                     }) {
                         Text("Сохранить")
-                    }
+                    } .disabled(todoText.isEmpty)
                 )
             }
+        }
+    }
+}
+
+struct ImportanceView: View {
+    @Binding var selectedImportance: Int
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Важность")
+                Spacer()
+                Picker(selection: $selectedImportance, label: Text("")) {
+                        Image(systemName: "arrow.down").tag(0)
+                        Text("нет").tag(1)
+                        Image(systemName: "exclamationmark.2")
+                            .foregroundColor(.red)
+                            .tag(2)
+                    }
+                    .frame(maxWidth: 150)
+                    .pickerStyle(SegmentedPickerStyle())
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+            }
+            .padding([.top, .leading, .trailing])
+        }
+    }
+}
+
+struct DeadlineView: View {
+    @Binding var isDeadlineOn: Bool
+    @Binding var isCalendarVisible: Bool
+    @Binding var deadlineDate: Date
+    
+    var body: some View {
+        Toggle(isOn: $isDeadlineOn) {
+            Text("Сделать до")
+            if isDeadlineOn {
+                Button(action: {
+                    isCalendarVisible.toggle()
+                }) {
+                    Text(DateFormatter.DateFormatter.string(from: deadlineDate))
+                }
+            }
+        } .padding([.bottom, .leading, .trailing])
+        
+        if isCalendarVisible {
+            DatePicker(
+                selection: $deadlineDate,
+                displayedComponents: [.date],
+                label: {}
+            )
+            .padding(.top, -20)
+            .padding([.leading, .trailing])
+            .datePickerStyle(GraphicalDatePickerStyle())
         }
     }
 }
